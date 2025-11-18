921644c48a529ccd59ad6629c204288c116d084b
omkar <omkarbagade16@gmail.com>
2025-11-18 21:59:46 +0530

    Initial Commit

---
+++ OmkarCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OmkarCard
+//
+//  Created by Omkar Vijay Bagade on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OmkarCard/OmkarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmkarCardApp.swift
+//  OmkarCard
+//
+//  Created by Omkar Vijay Bagade on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OmkarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
